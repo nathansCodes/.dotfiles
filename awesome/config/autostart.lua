18878113db1cd7e32315f2ec2f4454a3b8a681c1
NathansCodes <nat.schrei14@gmail.com>
2023-06-25 13:58:17 +0200

    awesome: autostart clipmenud

--- awesome/config/autostart.lua
+++ awesome/config/autostart.lua
@@ -4,7 +4,11 @@ local awful = require("awful")
 awful.spawn("/home/nathan/.screenlayout/default.sh")
 
 -- compositor
-awful.spawn.with_shell("picom")
+awful.spawn.with_shell("picom -b")
 
 -- wallpaper
 awful.spawn.with_shell("nitrogen --restore")
+
+-- clipboard
+awful.spawn.with_shell("CM_OWN_CLIPBOARD=1 clipmenud")
+
