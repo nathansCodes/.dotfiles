42dc25d873e3dfd5cba09f7a1a352d63ec69eccd
NathansCodes <nat.schrei14@gmail.com>
2023-06-25 13:48:46 +0200

    awesme: remove modkey from rc.lua

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -29,18 +29,6 @@ naughty.connect_signal("request::display_error", function(message, startup)
 end)
 -- }}}
 
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_configuration_dir() .. "ui/theme.lua")
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
-modkey = "Mod4"
--- }}}
-
 -- {{{ Notifications
 
 ruled.notification.connect_signal('request::rules', function()
