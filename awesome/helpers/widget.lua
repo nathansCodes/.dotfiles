071023877b435c0b7c24c975c25d144b4d24d240
NathansCodes <nat.schrei14@gmail.com>
2023-08-02 21:45:11 +0200

    awesome: widgets helper: add id and better top, bottom, right, left margins

--- awesome/helpers/widget.lua
+++ awesome/helpers/widget.lua
@@ -12,16 +12,15 @@ function format_item(widget)
                 layout = wibox.layout.align.vertical,
                 expand = 'none',
                 spacing = dpi(10),
-                id = widget.id,
                 nil,
                 widget,
                 nil
             },
             margins = widget.margins or dpi(10),
-            left = widget.left or dpi(10),
-            right = widget.right or dpi(10),
-            top = widget.top or dpi(10),
-            bottom = widget.bottom or dpi(10),
+            left    = widget.margins or widget.left or dpi(10),
+            right   = widget.margins or widget.right or dpi(10),
+            top     = widget.margins or widget.top or dpi(10),
+            bottom  = widget.margins or widget.bottom or dpi(10),
             widget = wibox.container.margin
         },
         forced_height = widget.forced_height or dpi(88),
@@ -48,21 +47,21 @@ function format_item_no_fix_height(widget)
                 nil
             },
             margins = widget.margins or dpi(10),
-            left = widget.left or dpi(10),
-            right = widget.right or dpi(10),
-            top = widget.top or dpi(10),
-            bottom = widget.bottom or dpi(10),
-            widget = wibox.container.margin
+            left    = widget.margins or widget.left or dpi(10),
+            right   = widget.margins or widget.right or dpi(10),
+            top     = widget.margins or widget.top or dpi(10),
+            bottom  = widget.margins or widget.bottom or dpi(10),
+            widget  = wibox.container.margin,
         },
-        forced_height = widget.forced_height or dpi(88),
+        forced_height = widget.forced_height,
         border_width  = widget.border_width or dpi(0),
         border_color  = widget.border_color or beautiful.border_normal,
         bg = widget.bg or beautiful.bg_focus .. beautiful.semi_transparent,
-        id = widget.id or "bg",
         shape = widget.shape or function(cr, width, height)
             gears.shape.rounded_rect(cr, width, height, 20)
         end,
         widget = wibox.container.background,
+        id = widget.id,
     }
 end
 
