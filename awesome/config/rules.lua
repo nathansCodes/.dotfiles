03319b3bbe6f7533e612809a868af364ba966a2f
naan187 <nat.schrei14@gmail.com>
2023-10-23 15:38:06 +0200

    awesome: make gimp opaque

--- awesome/config/rules.lua
+++ awesome/config/rules.lua
@@ -2,6 +2,12 @@ local awful = require("awful")
 
 awful.rules.rules = {
     {
+        rule = { class = "Gimp-.*" },
+        properties = {
+            opacity = 1.0,
+        }
+    },
+    {
         rule = { class = "Inkscape" },
         properties = {
             opacity = 1.0,
