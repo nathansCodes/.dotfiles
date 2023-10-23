842de219b6a1d8259b889696ca5ef130866ff8f8
naan187 <nat.schrei14@gmail.com>
2023-10-23 15:59:25 +0200

    rofi: fix powermenu.sh script to actually log out in awesome

--- rofi/powermenu/powermenu.sh
+++ rofi/powermenu/powermenu.sh
@@ -76,7 +76,7 @@ run_cmd() {
 				bspc quit
 			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
 				i3-msg exit
-			elif [[ "$DESKTOP_SESSION" == 'awesome' ]]; then
+			elif [[ "$DESKTOP_SESSION" == 'awesomex11' ]]; then
                 awesome-client "awesome.quit()"
 			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
 				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
