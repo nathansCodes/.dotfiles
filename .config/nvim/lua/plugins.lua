a046e8a475871b198dbec9804aef7d14de2b7e3e
Nathan Schreitl <nat.schrei14@gmail.com>
2024-02-10 17:12:58 +0100

    fix submods

--- .config/nvim/lua/plugins.lua
+++
@@ -1,6 +0,0 @@
-return {
-    "mbbill/undotree",
-    "christoomey/vim-tmux-navigator",
-    "tpope/vim-surround",
-    "rcarriga/nvim-notify",
-}
--- .config/nvim/lua/plugins/mason.lua
+++
@@ -1,4 +0,0 @@
-return {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate"
-}
