f8a88e38a989c3fe13a09e015655b2ca563a5e1c
Nathan Schreitl <nat.schrei14@gmail.com>
2024-04-09 16:20:58 +0200

    revert theming before shutting down and rebooting

--- .config/awesome/helpers/power.lua
+++ .config/awesome/helpers/power.lua
@@ -1,12 +1,18 @@
 local awful = require("awful")
 
-local capi = { awesome = awesome }
-
 local power = {}
 
-function power.shutdown() awful.spawn("systemctl poweroff") end
-function power.reboot() awful.spawn("systemctl reboot") end
-function power.lock() capi.awesome.emit_signal("lockscreen::lock") end
-function power.logout() capi.awesome.quit() end
+function power.shutdown()
+    require("ui.theme.themer").revert(function()
+        awful.spawn("systemctl poweroff")
+    end)
+end
+function power.reboot()
+    require("ui.theme.themer").revert(function()
+        awful.spawn("systemctl reboot")
+    end)
+end
+function power.lock() awesome.emit_signal("lockscreen::lock") end
+function power.logout() awesome.quit() end
 
 return power
